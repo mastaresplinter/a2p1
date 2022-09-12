b38545c3500e971e75c317753e88cc0a53b1ada8
mastaresplinter <mackandn@gmail.com>
2022-09-12 10:54:04 +0200

    First Commit
    First commit

---
+++ a2p1.c
@@ -0,0 +1,17 @@
+/*
+	Modified by Wagner Morais on Sep 2022.
+ */
+
+#include <stdio.h>
+#include "piface.h"
+#include "led.h"
+
+int main()
+{
+	piface_init();
+	// piface_clear();
+	// piface_puts("Short string");
+	// piface_puts("This is a long string");
+	// piface_puts("1st line\n2nd line");
+	return 0;
+}
